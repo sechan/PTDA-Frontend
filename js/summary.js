9571ff19cd343026b5d42f3ae5c0a96b44185383
Sean Chan <schan2@lakeheadu.ca>
2019-02-13 19:12:22 -0500

    Added emailing functionality to summary page

--- js/summary.js
+++ js/summary.js
@@ -3,6 +3,7 @@
 $(document).ready(function(){
   console.log("summary.js loaded.")
 
+  // Variables needed to create a PDF of the summary
   var doc = new jsPDF();
   var specialElementHandlers = {
     '#editor': function(element, renderer) {
@@ -10,8 +11,8 @@ $(document).ready(function(){
     }
   };
 
+  // Handlers for PDF export button
   $('#cmd').unbind("click");
-
   $('#cmd').click(function(){
     console.log("button clicked");
 
@@ -21,4 +22,27 @@ $(document).ready(function(){
     doc.save('summary.pdf');
     console.log("PDF created");
   });
+
+  var templateParams = {
+    name: 'Sean',
+    notes: 'check this out'
+  }
+
+  // Handlers for sending an email button
+  $('#email').unbind("click");
+  $('#email').click(function(){
+    console.log("Send email button clicked");
+    console.log("Email sent!");
+  });
+
+  // Sends email using EmailJS
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    console.log("Send button clicked!");
+    event.preventDefault();
+    this.contact_number.value = Math.random() * 100000 | 0;
+    emailjs.sendForm('gmail', 'contact_form', this);
+    console.log("email sent!");
+  });
+
+
 });
